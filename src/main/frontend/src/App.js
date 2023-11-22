6faf2bea4d1bc1eb05aef0565f296f1768c7d6e5
Luigi Verolla <luverolla@gmx.it>
2023-11-22 10:30:32 +0100

    frontend integration

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    var a = new Date().toString();
+    return (
+        <h1>
+            Date is {a}
+        </h1>
+    )
+}
+
+export default App;
